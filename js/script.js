742a9dae94b51841b8cb8c23db7ababe13380282
joaopsilv <joao_p_silveira@estudante.sc.senai.br>
2022-03-06 23:42:41 -0300

    starting to develop light-mode - 06/03

--- js/script.js
+++ js/script.js
@@ -1,35 +1,137 @@
 /* Navbar Movment */
-let navbar = document.querySelector("header")
-let position0 = window.scrollY
+let header = document.querySelector("header")
+let prevScroll = window.scrollY
 
 window.addEventListener("scroll", ()=>{
-    if(window.pageYOffset > position0){
-        navbar.classList.add("sticky")
+    if(window.pageYOffset > prevScroll && header.classList.contains("dark-header")){
+        header.classList.add("sticky")
+        header.classList.add("dark-sticky")
+    }
+    else if(window.pageYOffset > prevScroll && header.classList.contains("light-header")){
+        header.classList.add("sticky")
+        header.classList.add("light-sticky")
     }
     else{
-        navbar.classList.remove("sticky")
+        header.classList.remove("sticky")
     }
 
-    let currentscroll = window.pageYOffset
-    if(currentscroll > 625){
-        navbar.classList.add("out")
+    let currentScroll = window.pageYOffset
+    if(currentScroll > 625){
+        header.classList.add("out")
     }
     else{
-        navbar.classList.remove("out")
+        header.classList.remove("out")
     }
 })
 
 /* Switch Toggle */
-let switchtoggle = document.getElementsByClassName("switch-toggle")[0]
+let switchToggle = document.getElementsByClassName("switch-toggle")[0]
 let icon = document.querySelector(".fas")
 
-switchtoggle.addEventListener("click", ()=>{
+let body = document.querySelector("body")
+
+let sections = document.querySelectorAll(".content")
+
+let navItens = document.querySelectorAll(".navLink-color")
+let navTitle = document.querySelector(".navTitle-color")
+
+let main = document.querySelector(".main")
+let firstButton = document.querySelector(".first-button")
+let firstButtonAlter = document.querySelector(".first-button-alter")
+
+let aboutContent = document.querySelector(".text-about")
+let curric = document.querySelector(".text-about a")
+let aboutCards = document.querySelectorAll(".card-about article")
+let aboutCardsTitles = document.querySelectorAll(".card-about h1")
+let aboutCardsSubtitles = document.querySelectorAll(".card-about p")
+
+let projectTitle = document.querySelector(".title-projects h1")
+let projectsCards = document.querySelectorAll(".cards-projects article")
+let projectsCardsTitles = document.querySelectorAll(".card-projects h1")
+let projectsCardsSubtitles = document.querySelectorAll(".card-projects p")
+let projectsCardsImages = document.querySelectorAll(".card-projects img")
+
+switchToggle.addEventListener("click", ()=>{
+    /*Switch Toggle*/
     icon.classList.toggle("fa-moon")
     icon.classList.toggle("fa-circle")
     icon.classList.toggle("switch-animation")
-    switchtoggle.classList.toggle("changebg")
-})
+    switchToggle.classList.toggle("changebg")
 
+    /* Body - Config */
+    body.classList.toggle("dark-body")
+    body.classList.toggle("light-body")
+
+    /* Section - Config */
+    for (let i = 0; i < sections.length; i++){
+        sections[i].classList.toggle("dark-section")
+        sections[i].classList.toggle("light-section")
+    }
+
+    /* Header - Navbar */
+    header.classList.toggle("dark-header")
+    header.classList.toggle("light-header")
+    header.classList.remove("dark-sticky")
+    header.classList.remove("light-sticky")
+    for (let i = 0; i < navItens.length; i++){
+        navItens[i].classList.toggle("dark-font")
+        navItens[i].classList.toggle("light-font")
+    }
+    navTitle.classList.toggle("dark-font")
+    navTitle.classList.toggle("light-font")
+
+    /* Main */
+    main.classList.toggle("dark-font")
+    main.classList.toggle("light-font")
+    firstButton.classList.toggle("dark-button1")
+    firstButton.classList.toggle("light-button1")
+    firstButtonAlter.classList.toggle("dark-button2")
+    firstButtonAlter.classList.toggle("light-button2")
+
+    /* Section About */
+    aboutContent.classList.toggle("dark-font")
+    aboutContent.classList.toggle("light-font")
+    curric.classList.toggle("dark-font")
+    curric.classList.toggle("light-font")
+    for (let i = 0; i < aboutCards.length; i++){
+        aboutCards[i].classList.toggle("dark-card")
+        aboutCards[i].classList.toggle("light-card")
+    }
+    for (let i = 0; i < aboutCardsTitles.length; i++){
+        aboutCardsTitles[i].classList.toggle("dark-font")
+        aboutCardsTitles[i].classList.toggle("light-font")
+    }
+    for (let i = 0; i < aboutCardsSubtitles.length; i++){
+        aboutCardsSubtitles[i].classList.toggle("weakgrey-font")
+        aboutCardsSubtitles[i].classList.toggle("mediumgrey-font")
+    }
+        
+    /* Section Projects */
+    projectTitle.classList.toggle("dark-font")
+    projectTitle.classList.toggle("light-font")
+    for (let i = 0; i < projectsCards.length; i++){
+        projectsCards[i].classList.toggle("dark-card")
+        projectsCards[i].classList.toggle("light-card")
+    }
+    for (let i = 0; i < projectsCardsTitles.length; i++){
+        projectsCardsTitles[i].classList.toggle("dark-font")
+        projectsCardsTitles[i].classList.toggle("light-font")
+    }
+    for (let i = 0; i < projectsCardsSubtitles.length; i++){
+        projectsCardsSubtitles[i].classList.toggle("weakgrey-font")
+        projectsCardsSubtitles[i].classList.toggle("mediumgrey-font")
+    }
+    for (let i = 0; i < projectsCardsImages.length; i++){
+        projectsCardsImages[i].classList.toggle("dark-img")
+        projectsCardsImages[i].classList.toggle("light-img")
+    }
+
+    /* Section Knowledges */
+
+    
+    /* Footer - Credits */
+
+})
 
 /* Skills Cards */
 let explainment = document.querySelector("#card-mechanic")
